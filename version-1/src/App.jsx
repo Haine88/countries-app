9e30ee92cd83a9d1f3e733d48f4f1188054a1fb1
haine88 <pathamawanhanford@gmail.com>
2026-01-12 13:21:59 -0800

    set up for version-1

---
+++ version-1/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import SavedCountries from './pages/SavedCountries';
+import CountryDetails from './pages/CountryDetails.jsx';
+import localData from '../localData.js'
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">Where in the world?</Link>
+        <Link to="/saved">Saved Countries</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home countriesData={localData} />} />
+        <Route path="saved" element={<SavedCountries />} />
+        <Route path="/country/:name" element={<CountryDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

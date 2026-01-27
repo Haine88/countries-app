43b31545a3cf9d97944645fefc448f7f5cd18ca6
haine88 <pathamawanhanford@gmail.com>
2026-01-27 12:56:46 -0800

    add view count to countriesDetail page

--- version-2/src/pages/CountryDetails.jsx
+++ version-2/src/pages/CountryDetails.jsx
@@ -1,11 +1,36 @@
+import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 function CountryDetails({ countriesData }) {
     // get country's name from the URL parameter // 
     const countryName = useParams().countryName;
 
+    const [viewCount, setViewCount] = useState(0);
+
     // use .find() to find country that matches // 
     const country = countriesData.find(country => country.name.common == countryName);
+
+    const updateViewCount = async (countryName) => {
+        try {
+            const res = await fetch('/api/update-one-country-count', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    country_name: countryName
+                }),
+            });
+            const result = await res.json();
+            setViewCount(result.count);
+        } catch (error) {
+            console.log('error', error);
+        }
+    }
+
+    useEffect(() => {
+        updateViewCount(country.name.common);
+    }, []);
 
 
     const saveCountry = async (countryName) => {
@@ -47,6 +72,7 @@ function CountryDetails({ countriesData }) {
             <p>Population: {country.population.toLocaleString()}</p>
             <p>Region: {country.region}</p>
             <p>Capital: {country.capital?.[0]}</p>
+            <p>Viewed: {viewCount} times</p>
             </div>
             </div>
         </div>

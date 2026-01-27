d3d9c04cc8ed66a248afcece534c3368c1b0fad3
haine88 <pathamawanhanford@gmail.com>
2026-01-27 09:37:07 -0800

    add functionality to save button

--- version-2/src/pages/CountryDetails.jsx
+++ version-2/src/pages/CountryDetails.jsx
@@ -7,6 +7,28 @@ function CountryDetails({ countriesData }) {
     // use .find() to find country that matches // 
     const country = countriesData.find(country => country.name.common == countryName);
 
+
+    const saveCountry = async (countryName) => {
+        try {
+            const res = await fetch('/api/save-one-country', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    country_name: countryName,
+                }),
+            })
+            const result = await res.text();
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+
+    const handleSave = () => {
+        saveCountry(country.name.common)
+    }
+
     return (
         <div className="country-details-container">
             <Link to="/" className="back-button">
@@ -16,7 +38,11 @@ function CountryDetails({ countriesData }) {
                 <img src={country.flags.png} alt={`${country.name.common} flag`} />
                 <div className="country-detail-info">
                     <h1>{country.name.common}</h1>
-                    <button className="save-button">Save</button>
+                    <button
+                        className="save-button"
+                        onClick={handleSave}>
+                        Save
+                    </button>
             
             <p>Population: {country.population.toLocaleString()}</p>
             <p>Region: {country.region}</p>

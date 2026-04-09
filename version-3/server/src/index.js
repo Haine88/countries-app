bbd08736796ca701082d0a3e8afd57d2f51cb46f
haine88 <pathamawanhanford@gmail.com>
2026-04-09 10:36:50 -0700

    fix some bug in version-4

--- version-3/server/src/index.js
+++ version-3/server/src/index.js
@@ -4,7 +4,7 @@ import pg from "pg";
 import config from "./config.js";
 
 const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL + "&uselibpqcompat=true",
+  connectionString: config.DATABASE_URL + "&uselibpqcompat=true",
   ssl: true,
 });
 
--- version-4/server/src/index.js
+++ version-4/server/src/index.js
@@ -1,10 +1,9 @@
 
 import express from "express";
 import pg from "pg";
-import config from "./config.js";
 
 const db = new pg.Pool({
-  connectionString: config.databaseUrl + "&uselibpqcompat=true",
+  connectionString: process.env.DATABASE_URL + "&uselibpqcompat=true",
   ssl: true,
 });
 
